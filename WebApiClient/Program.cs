51b422acdc045a3c2f2a11d8ce27fa94c050d852
Catalina-Eleonora Prioteasa <Catalina-Eleonora.Prioteasa@lobyco.com>
2022-05-31 17:19:40 +0300

    Add project files.

---
+++ WebApiClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApiClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("press any key cont...");
+            Console.ReadLine();
+
+            using (System.Net.Http.HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("http://localhost:34063/values");
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    string messagee = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(messagee);
+                }
+                else
+                {
+                    Console.WriteLine($"response error code: {response.StatusCode}");
+                }    
+            }
+        }
+    }
+}
